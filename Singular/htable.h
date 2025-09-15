bcaeaf50ef7f2cb8be1026c1d2e9e3069e8d7718
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2025-09-15 10:48:45 +0200

    memory mgt. for htable

--- Singular/htable.h
+++ Singular/htable.h
@@ -25,15 +25,17 @@ struct stablerec
   int max;
   int ref;
 };
+#define TABLE_MAX 1000
 
 stablerec* t_createTable(int s);
 void t_destroyTable(stablerec* t);
 stablerec* copyTable(stablerec* t);
 char* stringTable(stablerec* t);
+void htable_Print(stablerec *d);
 /// find the entry to key s
 telem t_findTable(stablerec* t,const char *s);
 /// find the data to key s
 leftv t_findTabelVal(stablerec* t,const char *s);
 /// add a new entry (key s, data v) to table t
-void t_addTable(stablerec* t,const char *s, leftv v);
+void t_addTable(stablerec* t,char *s, leftv v);
 #endif
